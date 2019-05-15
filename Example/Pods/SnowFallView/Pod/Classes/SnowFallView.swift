58be79e036379e7fb01cdefd7e199e708e2e3420
Famara Kassama <>
2019-05-15 15:50:06 +0200

    pod install

---
+++ Example/Pods/SnowFallView/Pod/Classes/SnowFallView.swift
@@ -0,0 +1,117 @@
+//
+//  SnowFallView.swift
+//  SnowFallView
+//
+//  Copyright © 2016 Ha Nyung Chung. All rights reserved.
+//
+
+import CoreGraphics
+
+public class SnowFallView: UIView {
+    
+    var isSnowing = false
+    
+    public var density: Float = 4.0 {
+        didSet {
+            self.setUpEmitters()
+        }
+    }
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setUp()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.setUp()
+    }
+    
+    public func start() {
+        isSnowing = true
+        var depth = 0
+        
+        guard let sublayers = self.layer.sublayers as? [CAEmitterLayer] else {
+            return
+        }
+        
+        for emitter in sublayers {
+            emitter.birthRate = Float(15 + 5 * depth)
+            depth += 1
+        }
+    }
+    
+    public func stop() {
+        isSnowing = false
+        guard let sublayers = self.layer.sublayers as? [CAEmitterLayer] else {
+            return
+        }
+        
+        for emitter in sublayers {
+            emitter.birthRate = 0
+        }
+    }
+    
+    public override func didMoveToSuperview() {
+        self.setUpEmitters()
+    }
+    
+    func setUp() {
+        self.backgroundColor = .clear
+        self.layer.masksToBounds = true
+    }
+    
+    func setUpEmitters() {
+        guard self.superview != nil else {
+            return;
+        }
+        
+        self.layer.sublayers = nil;
+        
+        let snowFlake = CAEmitterCell()
+        snowFlake.birthRate = self.density
+        snowFlake.lifetime = 15
+        snowFlake.lifetimeRange = 5.0
+        
+        let bundle = Bundle(for: type(of: self))
+        let image = UIImage(named: "snowflake", in: bundle, compatibleWith: nil)
+        
+        snowFlake.contents = image?.cgImage
+        snowFlake.scale = 1.5 / UIScreen.main.scale
+        snowFlake.name = "snowflake"
+        snowFlake.velocity = 60
+        snowFlake.emissionLongitude = .pi
+        snowFlake.emissionLatitude = .pi / 4.0
+        snowFlake.spin = 0.7
+        
+        for i in 0..<3 {
+            let emitter = CAEmitterLayer()
+            emitter.position = CGPoint(x: self.center.x, y: 0)
+            emitter.emitterShape = CAEmitterLayerEmitterShape.line
+            emitter.emitterSize = CGSize(width: self.bounds.width * 1.5, height:1)
+            
+            let depth = Float(i) / 3.0
+            emitter.opacity = 1.0 - depth
+            emitter.scale = 1.0 - depth
+            emitter.velocity = 1.0 - depth
+            emitter.lifetime = 1.0 / (1.0 - depth)
+            
+            emitter.emitterCells = [snowFlake]
+            
+            self.layer.addSublayer(emitter)
+        }
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        if isSnowing {
+            setUpEmitters()
+        }
+    }
+    
+    override public func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        return false
+    }
+}
---
+++ Example/Pods/SnowFallView/README.md
@@ -0,0 +1,43 @@
+# SnowFallView
+
+[![CI Status](http://img.shields.io/travis/minorblend/SnowFallView.svg?style=flat)](https://travis-ci.org/minorblend/SnowFallView)
+[![Version](https://img.shields.io/cocoapods/v/SnowFallView.svg?style=flat)](http://cocoapods.org/pods/SnowFallView)
+[![License](https://img.shields.io/cocoapods/l/SnowFallView.svg?style=flat)](http://cocoapods.org/pods/SnowFallView)
+[![Platform](https://img.shields.io/cocoapods/p/SnowFallView.svg?style=flat)](http://cocoapods.org/pods/SnowFallView)
+
+![SnowFall](https://cloud.githubusercontent.com/assets/591914/15089614/8de8e2ee-1439-11e6-8a9c-efb200946542.gif)
+
+## Usage
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+* iOS 8.0+
+* Xcode 7.3+
+
+## Installation
+
+SnowFallView is available through [CocoaPods](http://cocoapods.org). To install it, simply add the following line to your Podfile:
+
+```ruby
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'SnowFallView', '~> 0.3'
+```
+
+Then, run the following command
+
+```
+$ pod install
+```
+
+## Author
+
+Josh Ha-Nyung Chung, minorblend@gmail.com
+
+## License
+
+SnowFallView is available under the MIT license. See the LICENSE file for more info.
